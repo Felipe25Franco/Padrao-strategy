6f2743addcf231babd681e70eba9df1beed6f1e0
Felipe Franco <felipejf739@hotmail.com>
2025-05-30 13:59:19 -0300

    iden

--- src/main/java/Opcao.java
+++ src/main/java/Opcao.java
@@ -1,11 +1,9 @@
 public class Opcao {
-
     private double valor;
 
     public Opcao(double valor) {
         this.valor = valor;
     }
-
     public double executarPagamento(OpcaoPagamento opcao) {
         return opcao.pagar(valor);
     }
--- src/main/java/OpcaoCartaoCredito.java
+++ src/main/java/OpcaoCartaoCredito.java
@@ -1,10 +1,6 @@
 public class OpcaoCartaoCredito implements OpcaoPagamento {
-
     private int numeroParcelas;
-
-
     private static final int maxParcelas = 10;
-
 
     public OpcaoCartaoCredito(int numeroParcelas) {
         if (numeroParcelas < 1 || numeroParcelas > maxParcelas) {
@@ -12,16 +8,11 @@ public class OpcaoCartaoCredito implements OpcaoPagamento {
         }
         this.numeroParcelas = numeroParcelas;
     }
-
     @Override
     public double pagar(double valor) {
-
         double valorParcela = valor / numeroParcelas;
-
-
         System.out.println("Pagamento de R$" + valor + " realizado com Cartão de Crédito.");
         System.out.println("Valor parcelado em " + numeroParcelas + " vezes de R$" + valorParcela);
-
         return valor;
     }
 }
